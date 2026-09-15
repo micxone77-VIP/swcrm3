d80cb7dd60800297347a79100bd08bd5078d40ab
Marcus <micxone77@gmail.com>
2026-09-15 13:47:10 +0800

    chore: remove 7 orphaned/dead pages
    Deleted files with no active route in App.jsx:
    - Dashboard.jsx (old Daily Command Center, replaced by Today.jsx)
    - VIPDetail.jsx / VIPList.jsx (replaced by current VIP pages)
    - CampaignsCountryTiered.jsx / CampaignsMixedWrapper.jsx / CampaignsUnifiedCreator.jsx
    - NotificationBell.jsx (duplicate/unused component)

--- src/pages/CampaignsCountryTiered.jsx
+++
@@ -1,9 +0,0 @@
-import Campaigns from './Campaigns'
-import CampaignsUnifiedCreator from './CampaignsUnifiedCreator'
-
-export default function CampaignsCountryTiered(){
-  return <>
-    <CampaignsUnifiedCreator />
-    <Campaigns />
-  </>
-}
